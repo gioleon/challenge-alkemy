66661653b09602907032cfb3cb9623fd1fabc7be
gioleon <giovannileon2017@gmail.com>
2022-07-08 09:47:29 -0500

    Adding a new function

--- data_normalization.py
+++ data_normalization.py
@@ -1,14 +1,48 @@
 """
 This script takes a csv file and normalize its content
 """
-
-import pandas as pd
 import re
+import os
+import pandas as pd
+from decouple import config
 
-df = pd.read_csv("museos\\2022-July\museos-7-7-2022", encoding="UTF-8")
-df2 = pd.read_csv("cines\\2022-July\cines-7-7-2022", encoding="UTF-8")
-df3 = pd.read_csv("bibliotecas\\2022-July\\bibliotecas-7-7-2022", encoding="UTF-8")
+from loggings import set_up_loggin
 
+# Logger
+logger = set_up_loggin(config("FILE_LOGGER_NAME"))
+
+
+def find_files():
+    """
+    This function finds CSV files of interest
+    
+    @return: a list with the lasted CSV files downloaded
+    """
+    logger.info("Search for the most recent files of interest started")
+    
+    # Defining path
+    folders = ["museos", "cines", "bibliotecas"] # Main folder
+     
+    subfolder = [] # subfolder
+    subfolder.append(os.listdir("museos")[-1])
+    subfolder.append(os.listdir("cines")[-1])
+    subfolder.append(os.listdir("bibliotecas")[-1])
+    
+    files = [] # CSV files
+    for i in range(3):
+        path = folders[i]
+        path = os.path.join(path, subfolder[i])
+        files.append(os.listdir(path)[-1])
+        
+    complete_path = []
+    for i in range(3):
+        complete_path.append(os.path.join(folders[i], subfolder[i], files[i]))
+
+    logger.info("Path of the most recent files found")  
+      
+    return complete_path 
+  
+    
 def get_col_of_insterest(df: pd.DataFrame):
     """
     This function takes a DataFrame object 
@@ -51,11 +85,23 @@ def get_col_of_insterest(df: pd.DataFrame):
     cols = list(col_names.values())
     cols.remove("domicilio") # As domicilio is twice, we are deleting one of them.
     
-    
     return df[cols] 
 
-    
-        
+
+def clean_columns(df: pd.DataFrame): 
+    pass
+
+     
 if __name__ == '__main__':
-    print(get_col_of_insterest(df3)["categoria"].unique())
-    # rename_cols(df)
+    # Finding path
+    complete_path = find_files()
+    
+    dfs = [] # List of dataframes object with the columns of interest
+    for i in complete_path:
+        # It create a filtered dataframe 
+        dfs.append(get_col_of_insterest(pd.read_csv(i))) 
+    
+    # concatening dataframes    
+    final_df = pd.concat([dfs[0], dfs[1]], ignore_index=True)
+    final_df = pd.concat([final_df, dfs[2]], ignore_index=True)
+  
